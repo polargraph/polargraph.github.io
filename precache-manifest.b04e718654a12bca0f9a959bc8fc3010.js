5a224a519ce7d973b5c9fc2830826663f398b41e
Daniel Feles <dfeles@gmail.com>
2019-04-18 14:43:32 +0200

    Updates

---
+++ precache-manifest.b04e718654a12bca0f9a959bc8fc3010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fd7d9e83da8f92774c1",
+    "url": "/static/css/main.f108618c.chunk.css"
+  },
+  {
+    "revision": "1fd7d9e83da8f92774c1",
+    "url": "/static/js/main.6b4a0d35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4cbaaf2cde4312c6104c",
+    "url": "/static/js/2.c7d52607.chunk.js"
+  },
+  {
+    "revision": "493691de0a13b60dc1d63a1cd6684f23",
+    "url": "/index.html"
+  }
+];
