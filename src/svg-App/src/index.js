e31a2e6a2bc0e2d94ea6ae541718acc943c5ceaf
Agoston Nagy <stc@binaura.net>
2019-04-21 17:31:48 +0200

    updated drawing build: gui controlled renderer size

--- src/svg-App/src/index.js
+++ src/svg-App/src/index.js
@@ -6,11 +6,18 @@ import * as dat from 'dat.gui';
 import SinusScene from './scenes/SinusScene.js';
 import * as utils from './utils.js';
 
+var Params = function() {
+    this.rendererWidth = 420;
+    this.rendererHeight = 594;
+}
 window.onload = function() {
-
+    let params = new Params();
+    
     const gui = new dat.GUI();
     gui.domElement.id = 'gui';
-    
+    let controllerWidth = gui.add(params, 'rendererWidth', 100, 3000);
+    let controllerHeight = gui.add(params, 'rendererHeight', 100, 3000);
+
     const scene = new THREE.Scene();
     const cScene = new SinusScene(gui);
     scene.add(cScene);
@@ -20,16 +27,15 @@ window.onload = function() {
     const renderer = new THREE.WebGLRenderer({antialias: true});
     //const rendererWidth = container.getBoundingClientRect().width;
     //const rendererHeight = container.getBoundingClientRect().height; //container.offsetHeight;
-    let rendererWidth = 420;
-    let rendererHeight = 594;
-    renderer.setSize( rendererWidth, rendererHeight ); 
+    
+    renderer.setSize( params.rendererWidth, params.rendererHeight ); 
     renderer.setClearColor( 0xffffff, 1);
 
 
     const stats = new Stats();
     stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 
-    let camera = new THREE.PerspectiveCamera( 45, rendererWidth / rendererHeight, 0.1, 10000 );
+    let camera = new THREE.PerspectiveCamera( 45, params.rendererWidth / params.rendererHeight, 0.1, 10000 );
     camera.position.z = 900;
 
     const controls = new OrbitControls(camera, renderer.domElement);
@@ -44,6 +50,21 @@ window.onload = function() {
 
         stats.begin();
         stats.end();
+
+        controllerWidth.onChange(function(value) {
+            renderer.setSize(params.rendererWidth, params.rendererHeight);
+            camera.aspect = params.rendererWidth/ params.rendererHeight;
+            camera.updateProjectionMatrix();
+            renderer.domElement.style.left = container.getBoundingClientRect().width/2 - params.rendererWidth/2 + 'px';
+        });
+
+        controllerHeight.onChange(function(value) {
+            renderer.setSize(params.rendererWidth, params.rendererHeight);
+            camera.aspect = params.rendererWidth/ params.rendererHeight;
+            camera.updateProjectionMatrix();
+            renderer.domElement.style.top = container.getBoundingClientRect().height/2 - params.rendererHeight/2 + 'px';
+        });
+
         window.requestAnimationFrame(onAnimationFrameHandler);
     }
     window.requestAnimationFrame(onAnimationFrameHandler);
@@ -53,6 +74,8 @@ window.onload = function() {
 
     let x, y;
 
+
+
     const onMouseMove = ( event ) => {
         x = ( event.clientX / window.innerWidth );
         y = ( event.clientY / window.innerHeight );
@@ -60,7 +83,7 @@ window.onload = function() {
 
     const onKeyDown = (event) => {
         if(event.key == "s") {
-            utils.saveAsSVG( cScene.getVertices(), camera, rendererWidth, rendererHeight, "export.svg" );
+            utils.saveAsSVG( cScene.getVertices(), camera, params.rendererWidth, params.rendererHeight, "export.svg" );
         }
     }
 
@@ -69,8 +92,8 @@ window.onload = function() {
 
     // ------------------------- html related -------------------------
     document.body.style.margin = 0;
-    renderer.domElement.style.left = container.getBoundingClientRect().width/2 - rendererWidth/2 + 'px';
-    renderer.domElement.style.top = container.getBoundingClientRect().height/2 - rendererHeight/2 + 'px';
+    renderer.domElement.style.left = container.getBoundingClientRect().width/2 - params.rendererWidth/2 + 'px';
+    renderer.domElement.style.top = container.getBoundingClientRect().height/2 - params.rendererHeight/2 + 'px';
     renderer.domElement.style.position = 'absolute';
     renderer.domElement.style.boxShadow = '10px 10px 19px 0px rgba(0,0,0,0.23)';
     container.appendChild( renderer.domElement );
