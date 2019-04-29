d759b38db6d1f83def7022a13a9f3253d8bbdb67
Daniel Feles <dfeles@gmail.com>
2019-04-29 14:32:42 +0200

    adding load script functionality

--- src/polargraph/src/App.js
+++ src/polargraph/src/App.js
@@ -5,7 +5,7 @@ import Contact from './Contact.js'
 import Home from './Home.js'
 import { Container } from 'react-bootstrap';
 import './App.css';
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -14,13 +14,13 @@ class App extends Component {
         <Header/>
 
 
-        <Router>
-          <div>
+        <HashRouter>
+          <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
-          </div>
-        </Router>
+          </Switch>
+        </HashRouter>
         <div id="canvas"> </div>
       </div>
     );
--- src/polargraph/src/Home.js
+++ src/polargraph/src/Home.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Dropdown, Container } from 'react-bootstrap';
+import Script from 'react-load-script'
 
+
+let scripts = ['/main.js', '/main.js', '/main.js', '/main.js'];
 class Home extends Component {
+  state = {
+    loadScript: 0
+  }
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = { value: '' };
+  }
   render() {
     return (
       <div className="Contact">
-        <div id="canvas"> </div>
+        <Container>
+          <Dropdown className="mt-2 mb-2">
+            <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic">
+              Load other script
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              { scripts.map((script, index) =>
+                <Dropdown.Item href={'/?script=' + index}> {script} </Dropdown.Item>
+              ) }
+            </Dropdown.Menu>
+          </Dropdown>
+        </Container>
+        <div id="canvas"> </div><
+          Script url={scripts[this.getUrlParameter('script')] }/>
       </div>
     );
   }
+
   componentDidMount () {
-    const script = document.createElement("script");
+    
+  }
 
-    script.src = process.env.PUBLIC_URL + "/main.js";
-    script.async = true;
+  getUrlParameter = (name) => {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    let results = regex.exec(window.location.href);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  };
 
-    document.body.appendChild(script);
-}
+  handleChange (e) {
+    this.setState({ loadScript: e });
+  }
 }
 
 export default Home;
